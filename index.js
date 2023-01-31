17446de1b29085c27755741effec6f642e24c1b8
zxy <gcoordinate@gmail.com>
2023-01-31 17:44:30 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 const fetch = require('node-fetch');
 
 http.createServer(async function (req, res) {
-    console.log(req.body);
+    console.log(req.query);
     res.write(JSON.stringify({result: 1}));
     res.end();
 }).listen(process.env.PORT || 3000);
